4fa4e014351343c92c6b305dbb82ca1fb6832e9f
onaumenko <naumenkoa85@gmail.com>
2021-12-29 11:44:39 +0200

    add new branch

---
+++ src/main/java/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    String name;
+    String email;
+}
---
+++ src/main/java/service/IUser.java
@@ -0,0 +1,9 @@
+package service;
+
+import dto.UserDTO;
+
+interface IUser {
+    UserDTO getUserById(Long id);
+
+    Long createUser(UserDTO data);
+}
